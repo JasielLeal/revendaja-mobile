b6d82fc9e4a654dedd74f972268424213cea2533
Jasiel <jasieloficial@hotmail.com>
2024-11-24 10:16:18 -0300

    feat: adicionado função de pesquisa

--- src/pages/appPages/store/components/stock/stock.tsx
+++ src/pages/appPages/store/components/stock/stock.tsx
@@ -1,86 +1,114 @@
-import { Text, TouchableOpacity } from "react-native";
+import { useState, useEffect } from "react";
+import { Text, TouchableOpacity, View, FlatList, ActivityIndicator } from "react-native";
 import { Store } from "../../store";
 import { Button } from "@/components/buttton";
-import { View } from "react-native";
 import { Input } from "@/components/input";
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
 import { StockItem } from "./components/stockItem";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { GetStock } from "./services/GetStock";
-import { useState } from "react";
-import { FlatList } from "react-native";
-
 
 export function Stock() {
+    const pageSize = 10;
+    const [searchTerm, setSearchTerm] = useState(""); // Valor digitado no input
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState(""); // Valor com debounce
 
-    const pageSize = 10
-    const [searchTerm, setSearchTerm] = useState('');
+    // Configurando debounce usando setTimeout
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedSearchTerm(searchTerm); // Atualiza o termo apenas após 500ms
+        }, 500);
 
-    const { data } = useInfiniteQuery({
-        queryKey: ["GetStock", searchTerm],
+        return () => {
+            clearTimeout(handler); // Limpa o timeout anterior
+        };
+    }, [searchTerm]);
+
+    // Query utilizando o termo debounced
+    const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isLoading } = useInfiniteQuery({
+        queryKey: ["GetStock", debouncedSearchTerm],
         queryFn: ({ pageParam = 0 }) => {
-            return GetStock({ pageSize, page: pageParam + 1, searchTerm })
+            return GetStock({
+                pageSize,
+                page: pageParam + 1,
+                searchTerm: debouncedSearchTerm,
+            });
         },
+        staleTime: 1000 * 60 * 5,
         getNextPageParam: (lastPage, allPages) => {
             const currentPage = allPages.length;
 
             if (currentPage < lastPage.data?.totalPages) {
-
                 return currentPage;
             } else {
                 return undefined; // Para a paginação quando a última página for atingida
             }
         },
-        initialPageParam: 0
-    }) //parei aqui
+        initialPageParam: 0,
+    });
 
-    const allStock = data?.pages.flatMap((page) => page.data.items) || []
-
-    console.log(allStock)
+    const allStock = data?.pages.flatMap((page) => page.data.items) || [];
 
     return (
-        <>
-            <Store>
-                <View className="px-5">
-                    <View className="flex flex-row items-center justify-between mt-5">
-                        <Text className="text-white text-xl font-semibold">
-                            Estoque
-                        </Text>
-                        <Button name="Adicionar Produto" />
-                    </View>
-                    <View className="mt-5 flex flex-row items-center justify-between w-full">
-                        <View className="w-5/6">
-                            <Input
-                                name="Buscar"
-                                placeholder="Buscar"
-                                value={searchTerm} // Valor controlado pelo estado
-                                onChangeText={(text) => setSearchTerm(text)}
-                            />
-                        </View>
-                        <TouchableOpacity className="bg-forenground p-2 rounded-xl">
-                            <Icon name="filter" color={"#fff"} size={25} />
-                        </TouchableOpacity>
-                    </View>
-                    <FlatList
-                        data={allStock}
-                        keyExtractor={(item) => item.id}
-                        style={{ marginBottom: 180 }}
-                        renderItem={({ item }) => {
-                            const productData = item.customProduct || item.product;
-
-                            return (
-                                <StockItem
-                                    name={productData?.name || "Produto Indefinido"}
-                                    price={item.customPrice || productData?.suggestedPrice || "0"}
-                                    brand={productData?.brand || "Marca Desconhecida"}
-                                    quantity={item.quantity || 0}
-                                    imageUrl={productData?.imgUrl || undefined}
-                                />
-                            );
-                        }}
-                    />
+        <Store>
+            <View className="px-5">
+                <View className="flex flex-row items-center justify-between mt-5">
+                    <Text className="text-white text-xl font-semibold">Estoque</Text>
+                    <Button name="Adicionar Produto" />
                 </View>
-            </Store>
-        </>
-    )
+                <View className="mt-5 flex flex-row items-center justify-between w-full">
+                    <View className="w-5/6">
+                        <Input
+                            name="Buscar"
+                            placeholder="Buscar"
+                            value={searchTerm} // Valor controlado pelo estado
+                            onChangeText={(text) => setSearchTerm(text)}
+                        />
+                    </View>
+                    <TouchableOpacity className="bg-forenground p-2 rounded-xl">
+                        <Icon name="filter" color={"#fff"} size={25} />
+                    </TouchableOpacity>
+                </View>
+
+                {
+                    isLoading ?
+                        <View className="flex justify-center mt-72">
+                            <ActivityIndicator size="small" color={"#FF7100"} />
+                        </View>
+                        :
+                        <FlatList
+                            data={allStock}
+                            keyExtractor={(item) => item.id}
+                            style={{ marginBottom: 180 }}
+                            renderItem={({ item }) => {
+                                const productData = item.customProduct || item.product;
+
+                                return (
+                                    <StockItem
+                                        name={productData?.name || "Produto Indefinido"}
+                                        price={item.customPrice || productData?.suggestedPrice || "0"}
+                                        brand={productData?.brand || "Marca Desconhecida"}
+                                        quantity={item.quantity || 0}
+                                        imageUrl={productData?.imgUrl || undefined}
+                                    />
+                                );
+                            }}
+                            onEndReached={() => {
+                                if (hasNextPage) {
+                                    fetchNextPage();
+                                }
+                            }}
+                            onEndReachedThreshold={1}
+                            ListFooterComponent={() => {
+                                if (isFetchingNextPage) {
+                                    return <ActivityIndicator size="small" color={"#FF7100"} />
+                                } else {
+                                    return null;
+                                }
+                            }}
+                        />
+                }
+            </View>
+        </Store>
+    );
 }
